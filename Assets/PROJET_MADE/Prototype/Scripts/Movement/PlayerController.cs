e18e819ba529af688d548635b70ca241a5ac4401
saraschs <sara.sanches@ensaama.net>
2026-05-01 16:35:55 +0200

    scène de base
    création d'un système de déplacement du player pour la scène test, implémentation de la caméra 1st person et système de rotation de la caméra avec le mouvement de la souris. Création d'un élément de UI "raw image" qui restranscris l'image captée par la caméra sur l'écran du joueur avec une basse résolution pour un effet pixellisé.

---
+++ Assets/PROJET_MADE/Prototype/Scripts/Movement/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private float _jumpForce = 200;
+    [SerializeField] private Rigidbody _rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var vel = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _speed;
+        vel.y = _rb.velocity.y;
+        _rb.velocity = vel;
+
+        if (Input.GetKeyDown(KeyCode.Space)) _rb.AddForce(Vector3.up * _jumpForce);
+    }
+}
